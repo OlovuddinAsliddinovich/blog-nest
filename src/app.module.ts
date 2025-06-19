e265e6eeac2e66b05b4a591faee30414e2b3125f
Olovuddin <olovuddinramiddinov26@gmail.com>
2025-06-19 18:43:08 +0500

    Save

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
